ff203511fea858094944e6338feac66dcf4bc644
Kittach Sodsri <Kittach060@users.noreply.github.com>
2025-10-06 13:27:02 +0700

    week8

---
+++ week8/main 1.js
@@ -0,0 +1,20 @@
+//Practice-1
+//กลุ่ม
+//060
+const addButton=document.getElementById('add')
+addButton.addEventListener('click',()=>{
+    const box = document.getElementById("box");
+    box.classList.add("bordered")
+})
+ 
+const removeButton=document.getElementById('remove')
+removeButton.addEventListener('click',()=>{
+    const box = document.getElementById("box")
+    box.classList.remove("bordered")
+})
+ 
+const toggleButton=document.getElementById('toggle')
+toggleButton.addEventListener('click',()=>{
+    const box = document.getElementById("box")
+    box.classList.toggle("bordered")
+})
