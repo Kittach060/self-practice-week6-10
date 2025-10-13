0d90e32d5be54510d24f578e24d98d375e236398
Kittach Sodsri <Kittach060@users.noreply.github.com>
2025-10-13 19:51:27 +0700

    week9

---
+++ week9/main 2.js
@@ -0,0 +1,16 @@
+//060
+//กลุ่ม
+// practice 2
+const inputEle = document.getElementById('keyInput')
+const keyLog = document.getElementById('keyLog')
+ 
+inputEle.addEventListener('keydown', (event) =>{
+    const p = document.createElement('p')
+    if(event.key === 'Enter'){
+        p.textContent  = "You pressed: Enter "
+        p.style.color = "blue"
+    } else {
+        p.textContent = `You pressed: ${event.key}`
+    }
+    keyLog.appendChild(p)
+})
